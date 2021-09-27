f56f3a551e71f6aaf1b4a4061399abde91db41b9
Joël <joel.galeran@gmail.com>
2021-09-27 16:28:36 +0200

    test: update snapshots

--- packages/client/src/__tests__/integration/errors/missing-binary/test.ts
+++ packages/client/src/__tests__/integration/errors/missing-binary/test.ts
@@ -44,9 +44,13 @@ test('missing-binary', async () => {
       await prisma.user.findMany()
     }).rejects.toThrowErrorMatchingInlineSnapshot(`
 
-            Invalid \`prisma.user.findMany()\` invocation:
+            Invalid \`prisma.user.findMany()\` invocation in
+            /client/src/__tests__/integration/errors/missing-binary/test.ts:44:25
 
-
+              41 // When updating snapshots this is sensitive to OS, here Linux
+              42 // macOS will update extension to .dylib.node, but we need to kepp .so.node for CI
+              43 await expect(async () => {
+            → 44   await prisma.user.findMany(
               Query engine library for current platform "TEST_PLATFORM" could not be found.
             You incorrectly pinned it to TEST_PLATFORM
 
@@ -78,32 +82,32 @@ test('missing-binary', async () => {
       await prisma.user.findMany()
     }).rejects.toThrowErrorMatchingInlineSnapshot(`
 
-                        Invalid \`prisma.user.findMany()\` invocation:
+                                    Invalid \`prisma.user.findMany()\` invocation:
 
 
-                          Query engine binary for current platform "TEST_PLATFORM" could not be found.
-                        This probably happens, because you built Prisma Client on a different platform.
-                        (Prisma Client looked in "/client/src/__tests__/integration/errors/missing-binary/node_modules/@prisma/client/runtime/query-engine-TEST_PLATFORM")
+                                      Query engine binary for current platform "TEST_PLATFORM" could not be found.
+                                    This probably happens, because you built Prisma Client on a different platform.
+                                    (Prisma Client looked in "/client/src/__tests__/integration/errors/missing-binary/node_modules/@prisma/client/runtime/query-engine-TEST_PLATFORM")
 
-                        Searched Locations:
+                                    Searched Locations:
 
-                          /client/src/__tests__/integration/errors/missing-binary/node_modules/.prisma/client
-                          /client/src/__tests__/integration/errors/missing-binary/node_modules/@prisma/client/runtime
-                          /client/src/__tests__/integration/errors/missing-binary/node_modules/@prisma/client
-                          /client/src/__tests__/integration/errors/missing-binary/node_modules/.prisma/client
-                          /client/src/__tests__/integration/errors/missing-binary
-                          /tmp/prisma-engines
-                          /client/src/__tests__/integration/errors/missing-binary/node_modules/.prisma/client
+                                      /client/src/__tests__/integration/errors/missing-binary/node_modules/.prisma/client
+                                      /client/src/__tests__/integration/errors/missing-binary/node_modules/@prisma/client/runtime
+                                      /client/src/__tests__/integration/errors/missing-binary/node_modules/@prisma/client
+                                      /client/src/__tests__/integration/errors/missing-binary/node_modules/.prisma/client
+                                      /client/src/__tests__/integration/errors/missing-binary
+                                      /tmp/prisma-engines
+                                      /client/src/__tests__/integration/errors/missing-binary/node_modules/.prisma/client
 
 
-                        To solve this problem, add the platform "TEST_PLATFORM" to the "binaryTargets" attribute in the "generator" block in the "schema.prisma" file:
-                        generator client {
-                          provider      = "prisma-client-js"
-                          binaryTargets = ["native"]
-                        }
+                                    To solve this problem, add the platform "TEST_PLATFORM" to the "binaryTargets" attribute in the "generator" block in the "schema.prisma" file:
+                                    generator client {
+                                      provider      = "prisma-client-js"
+                                      binaryTargets = ["native"]
+                                    }
 
-                        Then run "prisma generate" for your changes to take effect.
-                        Read more about deploying Prisma Client: https://pris.ly/d/client-generator
-                    `)
+                                    Then run "prisma generate" for your changes to take effect.
+                                    Read more about deploying Prisma Client: https://pris.ly/d/client-generator
+                              `)
   }
 })
